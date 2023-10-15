3ae600313109804157e72ce19cb024a0b45774a6
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-10-15 11:38:37 +0100

    Stupid way of paiting

--- Assets/Code/Painting/PaintSourceComponent.cs
+++ Assets/Code/Painting/PaintSourceComponent.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class PaintSourceComponent : MonoBehaviour
 {
 
-	[Tooltip("The object that represents the paint that will be sprayed on objects.")]
-	public GameObject paintPrefab;
+	[Tooltip("The object that represents the paint that will be sprayed on static objects.")]
+	public GameObject staticPaint;
 
     [Tooltip("The hitbox that should be used to determine whether or not we should paint a surface. Anything colliding with this hitbox will be painted.")]
     public QueryableHitboxComponent paintHitbox;
 
 	private void Start()
 	{
+		var staticLevel = new ContactFilter2D();
+		staticLevel.SetLayerMask(LayerMask.NameToLayer("StaticLevel"));
+		
+		var dynamicLevel = new ContactFilter2D();
+		dynamicLevel.SetLayerMask(LayerMask.NameToLayer("DynamicLevel"));
+		
 		paintHitbox.onNewCollisionEnter += collisionContact =>
 		{
-			Instantiate(paintPrefab, collisionContact.point, Quaternion.identity);
+			if (collisionContact.collider.IsTouching(staticLevel))
+			{
+				staticPaint.SetActive(true);
+			}
+		};
+		
+		paintHitbox.onCollisionEnter += collisionContact =>
+		{
+			if (collisionContact.collider.IsTouching(staticLevel))
+			{
+				staticPaint.SetActive(true);
+			}
+		};
+
+		
+		paintHitbox.onCollisionExit += _ =>
+		{
+			staticPaint.SetActive(false);
 		};
 	}
 
