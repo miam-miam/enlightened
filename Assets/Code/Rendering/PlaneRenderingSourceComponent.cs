7e4f9923bc513baaaea8da39b4a1acd6bd50873e
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2023-10-20 20:58:56 +0100

    Fixes level transitions

--- Assets/Code/Rendering/PlaneRenderingSourceComponent.cs
+++ Assets/Code/Rendering/PlaneRenderingSourceComponent.cs
@@ -12,19 +12,36 @@ public class PlaneRenderingSourceComponent : MonoBehaviour
     [Tooltip("The list of planemasters to instantiate on load.")]
     public PlaneMasterController planeMasters;
 
-    [Tooltip("The canvas that provides the thing we will be rendering to.")]
+	[Tooltip("The canvas that provides the thing we will be rendering to.")]
     public Canvas renderSource;
 
     internal static List<PlaneRenderedComponent> staged = new List<PlaneRenderedComponent>();
 
+    private static bool initialised = false;
+
+    private static Canvas renderSourceInUse;
+
+	private static PlaneMasterController planeMasterController;
+
 	// Start is called before the first frame update
 	void Start()
     {
+        if (initialised)
+        {
+            Destroy(renderSource.gameObject);
+            Destroy(gameObject);
+            return;
+		}
+        else
+		{
+			DontDestroyOnLoad(renderSource.gameObject);
+			renderSourceInUse = renderSource;
+		}
+        initialised = true;
 
-        
-
-        Camera mainCamera = GetComponent<Camera>();
+		Camera mainCamera = GetComponent<Camera>();
         mainCamera.transform.position = new Vector3(0, 0, -0.5f);
+        DontDestroyOnLoad(mainCamera);
 
 		mainCamera.nearClipPlane = 0;
         mainCamera.farClipPlane = 1f;
@@ -32,8 +49,10 @@ public class PlaneRenderingSourceComponent : MonoBehaviour
 		int current = 1;
 
         GameObject layoutController = new GameObject("Plane Debugger Layout");
-        layoutController.transform.parent = renderSource.transform;
+        layoutController.transform.parent = renderSourceInUse.transform;
 		VerticalLayoutGroup layoutGroup = layoutController.AddComponent<VerticalLayoutGroup>();
+
+        planeMasterController = planeMasters;
 
 		// Create all cameras for our plane masters
 		foreach (PlaneMaster planeMaster in planeMasters.planes.OrderBy(x => x.planeLayer))
@@ -41,7 +60,7 @@ public class PlaneRenderingSourceComponent : MonoBehaviour
             planeMaster.InitialiseRendering(planeMasters);
 			GameObject createdObject = new GameObject($"Plane Master: {planeMaster.name}");
             createdObject.transform.parent = transform;
-            Camera camera = createdObject.AddComponent<Camera>();
+			Camera camera = createdObject.AddComponent<Camera>();
 			camera.CopyFrom(mainCamera);
 			camera.nearClipPlane = current;
             camera.farClipPlane = current + 1;
@@ -55,7 +74,7 @@ public class PlaneRenderingSourceComponent : MonoBehaviour
             if (planeMaster.drawToScreen)
             {
                 GameObject rs = new GameObject($"Render Source: {planeMaster.name}");
-                rs.transform.parent = renderSource.transform;
+                rs.transform.parent = renderSourceInUse.transform;
 				RawImage createdCanvas = rs.AddComponent<RawImage>();
 				createdCanvas.texture = planeMaster.OutputRenderTexture;
                 (createdCanvas.transform as RectTransform).anchorMin = new Vector2(0, 0);
