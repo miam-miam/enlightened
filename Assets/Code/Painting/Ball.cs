ae724b509be090266a2173c8ea81bf46c3bfefd5
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-10-27 09:06:11 +0100

    Add ball throwing (#15)
    * Changes
    
    * Add dotted line for line renderer
    
    * Take into account gravity when throwing
    
    * Actually throw an exploding ball

---
+++ Assets/Code/Painting/Ball.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace Code.Painting
+{
+    public class Ball : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private GameObject explosionPrefab;
+        private bool isGhost;
+
+        public void Init(Vector3 velocity, bool isGhost) {
+            this.isGhost = isGhost;
+            rb.velocity = velocity;
+        }
+
+        public void OnCollisionEnter2D(Collision2D col) {
+            if (isGhost) return;
+            Instantiate(explosionPrefab, col.contacts[0].point, Quaternion.FromToRotation(transform.up, col.contacts[0].normal));
+            Destroy(gameObject);
+        }
+    }
+}
