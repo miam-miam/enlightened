a659e14104a9771ee6fe14cc4dda362b221f64eb
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-11-01 16:41:29 +0000

    Add targets

--- Assets/Code/Painting/Ball.cs
+++ Assets/Code/Painting/Ball.cs
@@ -8,8 +8,10 @@ namespace Code.Painting
         [SerializeField] private Rigidbody2D rb;
         [SerializeField] private GameObject explosionPrefab;
         private bool isGhost;
+        private int dynamicLevelLayer;
 
         public void Init(Vector3 velocity, bool isGhost) {
+            dynamicLevelLayer = LayerMask.NameToLayer("DynamicLevel");
             this.isGhost = isGhost;
             rb.velocity = velocity;
         }
@@ -17,6 +19,15 @@ namespace Code.Painting
         public void OnCollisionEnter2D(Collision2D col) {
             if (isGhost) return;
             Instantiate(explosionPrefab, rb.transform.position, Quaternion.FromToRotation(transform.up, col.contacts[0].normal));
+            
+            if (col.gameObject.layer == dynamicLevelLayer)
+            {
+                var paintable = col.gameObject.GetComponent<Paintable>();
+                if (paintable != null)
+                {
+                    paintable.onPaint(QueryableHitboxComponent.CalculateContactInformation(col.collider, transform.position));
+                }
+            }
             Destroy(gameObject);
         }
     }
