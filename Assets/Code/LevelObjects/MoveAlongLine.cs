60dcd68ce0efad2de182f83ecfaef90815107505
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-11-06 15:15:57 +0000

    Allow move along to be activated

--- Assets/Code/LevelObjects/MoveAlongLine.cs
+++ Assets/Code/LevelObjects/MoveAlongLine.cs
@@ -6,9 +6,14 @@ public class MoveAlongLine : MonoBehaviour
 {
     public LineRenderer lineToFollow;
 
+    [Tooltip("Add a target to activate the movement once the target is activated.")]
+    public Target target;
+
     private Vector3[] positions;
 
     private int index = 0;
+
+    private bool activated = true;
 
     public float speed = 3f;
     // Start is called before the first frame update
@@ -17,11 +22,25 @@ public class MoveAlongLine : MonoBehaviour
         positions = new Vector3[lineToFollow.positionCount];
         lineToFollow.GetPositions(positions);
         transform.position = new Vector3(positions[0].x, positions[0].y, transform.position.z);
+
+        if (target != null)
+        {
+            activated = false;
+            target.onActivate += () =>
+            {
+                activated = true;
+            };
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!activated)
+        {
+            return;
+        }
+        
         var targetPosition = new Vector3(positions[index].x, positions[index].y, transform.position.z); 
         transform.position = Vector3.MoveTowards(transform.position,
             targetPosition,
@@ -35,6 +54,10 @@ public class MoveAlongLine : MonoBehaviour
 
     public Vector3 GetPositionDelta()
     {
+        if (!activated)
+        {
+            return new Vector3();
+        }
         var targetPosition = new Vector3(positions[index].x, positions[index].y, transform.position.z); 
         var newPosition = Vector3.MoveTowards(transform.position,
             targetPosition,
