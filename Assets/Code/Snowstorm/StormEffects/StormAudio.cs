fd4789ea388001e1061ff226e8dc4af3b370c17a
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2023-11-12 14:59:39 +0000

    ambience (#40)

---
+++ Assets/Code/Snowstorm/StormEffects/StormAudio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+internal class StormAudio : SnowstormEffectComponent
+{
+
+	private void Start()
+	{
+		AudioSource audioSource = GetComponent<AudioSource>();
+		float maxVolume = audioSource.volume;
+		audioSource.volume = 0;
+		onTick += (power, real) =>
+		{
+			if (!real)
+				return;
+			audioSource.volume = power * maxVolume;
+		};
+		onEnd += () =>
+		{
+			audioSource.volume = 0;
+		};
+	}
+
+}
